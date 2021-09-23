d5c67e1bc28facc5a5d8ebb918bf0eac5642777d
Alexey Perevalov <alexey.perevalov@huawei.com>
2021-09-23 14:08:06 +0000

    Set ambient capability from CRI to OCI spec
    This patch uses special field "ambient" in CRI's capability
    structure.
    
    Signed-off-by: Alexey Perevalov <alexey.perevalov@huawei.com>

--- pkg/cri/opts/spec_linux.go
+++ pkg/cri/opts/spec_linux.go
@@ -389,6 +389,13 @@ func WithCapabilities(sc *runtime.LinuxContainerSecurityContext, allCaps []strin
 		caps = append(caps, "CAP_"+strings.ToUpper(c))
 	}
 	opts = append(opts, oci.WithDroppedCapabilities(caps))
+
+	caps = []string{}
+	for _, c := range capabilities.GetAmbientCapabilities() {
+		caps = append(caps, "CAP_"+strings.ToUpper(c))
+	}
+	opts = append(opts, oci.WithAmbientCapabilities(caps))
+
 	return oci.Compose(opts...)
 }
 
